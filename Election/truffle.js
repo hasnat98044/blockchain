0eda479b2fa3c8d3c8f5c022094875dab53e473c
Hasnat Ahmed <32216601+hasnat98044@users.noreply.github.com>
2018-10-05 05:15:51 -0700

    Add files via upload

---
+++ Election/truffle.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
+  networks: {
+    rinkeby: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: 4 // Match any network id
+    }
+  }
+};
